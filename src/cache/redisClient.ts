fb168a478a4e0b74d7585a471aa9abbb23d955df
Pavol Vernarec <pver@paymorrow.com>
2024-02-08 17:12:23 +0100

    redis client config edit

--- src/cache/redisClient.ts
+++ src/cache/redisClient.ts
@@ -2,9 +2,6 @@ import { createClient } from 'redis'
 
 export const client = createClient({
     url: process.env.REDIS_URL,
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
-    name: process.env.REDIS_NAME,
 })
 
 client.on('error', (err) => console.log('Redis Client Error', err))
